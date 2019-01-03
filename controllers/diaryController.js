9efbb9ec5e2442b23ca7300499dab5323009da2c
okezieobi <obiedereokezie@gmail.com>
2019-01-03 09:58:41 +0100

    [Feature #162880154] Built and tested update-one-entry endpoint

--- controllers/diaryController.js
+++ controllers/diaryController.js
@@ -1,5 +1,34 @@
 import diaryList from '../models/diaryModel';
 
-class DiaryController {}
+class DiaryController {
+  static updateOne(req, res) {
+    const id = parseInt(req.params.id, 10);
+    const diaryEntry = diaryList.find(diaryItem => diaryItem.id === id);
+    if (!diaryEntry) {
+      res.status(404).json({
+        message: 'Diary entry does not exist',
+      });
+    } else if (!req.body.title) {
+      res.status(400).json({
+        message: 'Title is required',
+      });
+    } else if (!req.body.description) {
+      res.status(400).json({
+        message: 'Description is required',
+      });
+    } else {
+      const updatedDiaryEntry = {
+        id: diaryEntry.id,
+        title: req.body.title,
+        description: req.body.description,
+      };
+      diaryList.splice(diaryEntry.id, 1, updatedDiaryEntry);
+      res.status(200).json({
+        message: 'Diary entry updated successfully',
+        updatedDiaryEntry,
+      });
+    }
+  }
+}
 
 export default DiaryController;
