58b3737f68049502ee038fb23619ae7856112f7a
okezieobi <obiedereokezie@gmail.com>
2019-01-03 09:16:54 +0100

    [Feature #162898194] Built and tested create-one-entry endpoint

--- controllers/diaryController.js
+++ controllers/diaryController.js
@@ -1,5 +1,28 @@
 import diaryList from '../models/diaryModel';
 
-class DiaryController {}
+class DiaryController {
+  static createOne(req, res) {
+    if (!req.body.title) {
+      res.status(400).json({
+        message: 'Title is required',
+      });
+    } else if (!req.body.description) {
+      res.status(400).json({
+        message: 'Description is required',
+      });
+    } else {
+      const diaryEntry = {
+        id: diaryList.length,
+        title: req.body.title,
+        description: req.body.description,
+      };
+      diaryList.push(diaryEntry);
+      res.status(201).json({
+        message: 'Diary Entry successfully created',
+        diaryEntry,
+      });
+    }
+  }
+}
 
 export default DiaryController;
