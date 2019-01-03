93bd6b53ea84069a7a923015430818e1834d8935
okezieobi <obiedereokezie@gmail.com>
2019-01-03 10:05:30 +0100

    [Feature #162883631] Built and tested delete-one-entry endpoint

--- controllers/diaryController.js
+++ controllers/diaryController.js
@@ -1,5 +1,20 @@
 import diaryList from '../models/diaryModel';
 
-class DiaryController {}
+class DiaryController {
+  static deleteOne(req, res) {
+    const id = parseInt(req.params.id, 10);
+    const diaryEntry = diaryList.find(diaryItem => diaryItem.id === id);
+    if (!diaryEntry) {
+      res.status(404).json({
+        message: 'Diary entry does not exist',
+      });
+    } else {
+      diaryList.splice(diaryEntry.id, 1);
+      res.status(200).json({
+        message: 'Diary entry successfully deleted',
+      });
+    }
+  }
+}
 
 export default DiaryController;
--- tests/diaryTest.js
+++ tests/diaryTest.js
@@ -5,3 +5,28 @@ import chaiHttp from 'chai-http';
 import app from '../index';
 
 chai.use(chaiHttp);
+
+describe("Test endpoint at '/v1/entries/:id to delete a diary entry with DELETE", () => {
+  it("it should delete a diary entry at '/v1/entries/:id' with DELETE if id exists", (done) => {
+    const id = 0;
+    chai.request(app)
+      .del(`/v1/entries/${id}`)
+      .then((res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('message').equal('Diary entry successfully deleted');
+        done();
+      });
+  });
+
+  it("should NOT delete a diary entry at '/v1/entries/:id' with DELETE if id does not exist", (done) => {
+    const id = 10;
+    chai.request(app)
+      .del(`/v1/entries/${id}`)
+      .then((res) => {
+        expect(res).to.have.status(404);
+        expect(res.body).to.have.property('message').equal('Diary entry does not exist');
+        done();
+      });
+  });
+});
