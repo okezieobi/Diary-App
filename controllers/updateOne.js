abca40092ba564d179c023405d965c9a39c37607
okezieobi <obiedereokezie@gmail.com>
2019-01-12 10:34:23 +0100

    [Feature #162880154] Built and tested an endpoint that updates a diary entry using an id

--- controllers/updateOne.js
+++ controllers/updateOne.js
@@ -1,5 +1,32 @@
 import diaries from '../models/diaries';
 
-function updateOneDiaryEntry() {}
+function updateOneDiaryEntry(req, res) {
+  const id = parseInt(req.params.id, 10);
+  const diaryEntry = diaries.find(diaryItem => diaryItem.id === id);
+  if (!diaryEntry) {
+    res.status(404).send({
+      message: 'Diary entry does not exist',
+    });
+  } else if (!req.body.title) {
+    res.status(400).send({
+      message: 'Title is required',
+    });
+  } else if (!req.body.description) {
+    res.status(400).send({
+      message: 'Description is required',
+    });
+  } else {
+    const updatedDiaryEntry = {
+      id: diaryEntry.id,
+      title: req.body.title,
+      description: req.body.description,
+    };
+    diaries.splice(diaryEntry.id, 1, updatedDiaryEntry);
+    res.status(200).send({
+      message: 'Diary entry updated successfully',
+      updatedDiaryEntry,
+    });
+  }
+}
 
 export default updateOneDiaryEntry;
