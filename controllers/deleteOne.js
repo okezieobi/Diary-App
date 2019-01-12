e8e2735a87d1c70a27547a9e30452d8ad67983de
okezieobi <obiedereokezie@gmail.com>
2019-01-12 11:22:22 +0100

    [Feature #162883631] Built and tested an endpoint that deletes a diary entry using an id

--- controllers/deleteOne.js
+++ controllers/deleteOne.js
@@ -1,5 +1,18 @@
 import diaries from '../models/diaries';
 
-function deleteOneDiaryEntry() {}
+function deleteOneDiaryEntry(req, res) {
+  const id = parseInt(req.params.id, 10);
+  const diaryEntry = diaries.find(diaryItem => diaryItem.id === id);
+  if (!diaryEntry) {
+    res.status(404).send({
+      message: 'Diary entry does not exist',
+    });
+  } else {
+    diaries.splice(diaryEntry.id, 1);
+    res.status(200).send({
+      message: 'Diary entry successfully deleted',
+    });
+  }
+}
 
 export default deleteOneDiaryEntry;
